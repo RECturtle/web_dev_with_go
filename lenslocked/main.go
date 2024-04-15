1313acabd1c2124134aeb9aa2ba39d5f57f00f12
RECturtle <spencer.mertes@gmail.com>
2024-04-15 08:05:21 -0400

    feat: add middleware to router

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,11 +36,21 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
+func paramHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	param := chi.URLParam(r, "param")
+	fmt.Fprint(w, "<p>Welcome user: ", param, "</p>")
+}
+
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/param/{param}", paramHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
