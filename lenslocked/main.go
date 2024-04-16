a3954edc5a990030bb5c8433eefbb6956820427d
RECturtle <spencer.mertes@gmail.com>
2024-04-16 17:34:43 -0400

    feat: added page templates and created rendering function

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,45 +2,46 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my amazing site!</h1>")
+	t, err := template.ParseFiles(filepath)
+
+	if err != nil {
+		log.Printf("Parsing template: %v", err)
+		http.Error(w, "There was an error rendering the page", http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template: %v", err)
+		http.Error(w, "There was an error rendering the page", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `
-	<h1>Wecome to the contact page!</h1>
-	<p>To get in touch email me at <a href="mailto:spencer.mertes@gmail.com">spencer.mertes@gmail.com</a>.
-	`)
+	tPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `
-	<h1>Frequently Asked Questions</h1>
-	<p>
-	<b>Q: Is there a free version?</b>
-	<li>Yes! We offer a 30 day free trial</li>
-	</p>
-	<p>
-	<b>Q: Is this the coolest website?</b>
-	<li>Of course, no question about it</li>
-	</p>
-	`)
-}
-
-func paramHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	param := chi.URLParam(r, "param")
-	fmt.Fprint(w, "<p>Welcome user: ", param, "</p>")
+	tPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tPath)
 }
 
 func main() {
@@ -50,7 +51,7 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
-	r.Get("/param/{param}", paramHandler)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
